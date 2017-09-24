e58a46ec9bb755e185752966a7bb996e8b72769b
Jason Hamilton <jhamilton3312@gmail.com>
2017-09-23 23:47:30 -0700

    Initial commit

---
+++ ns2d.py
@@ -0,0 +1,50 @@
+''' 2D Compressible Navier-Stokes Finite Volume Solver
+
+    This code is a DNS solver for the 2 dimentional Navier-Stokes equations
+    using the finite volume method. The solver uses single block structured
+    grids.
+
+    Temporal Schemes
+    - 2nd Order MacCormack Predictor-Corrector
+
+    Spacial Schemes
+    - 2nd Order Central MacCormack
+
+    Thermodynamics Models
+    - Calorical Perfect Gas (CPG)
+
+    Transport Models
+    - Sutherland's Law
+
+    Boundar Conditions
+    - Inlets
+        + Dirchlet: prescribe constant values for velocity, temperature,
+                    and pressure; density is computed from thermodynamics
+    - Outlets
+        + Neumann: gradients at outlet face set to zero.
+                   ghost cell set to interier cells
+    - Walls
+        + No slip adiabatic: velocity at face set to zero,
+                             temperature gradients at face set to zero.
+        + Slip adiabatic: velocity gradients at face set to zero,
+                          temperature gradients at face set to zero.
+    Development Notes:
+    - Add Species Equation
+    - Add TPG to Thermodynamics Models
+    - Add 3rd Order MUSCL to Spacial Schemes
+    - Add chemical reactions/kinetics
+    - Add RK4 to Temporal Schemes
+    - Add Peng-Robinson to Thermodynamics Models
+    - Add Chungs's (1988) high density model
+    - Add input file support (most likely xml format)
+
+    This is code is being developed as a learning tool. Will likely be
+    rewitten as sepereate program when transitioned into 3D. Once 3D single
+    block had been validated/verified then multiblock functionality will be
+    added. The goal is grain a better understanding of core principles in CFD
+    and scientific computing, as well as improving programming efficiency.
+
+    The end product should provide a platform for rapid model development and
+    testing. Once new models/features are tested using python solvers they can
+    be implimented into fortran solvers.
+'''
